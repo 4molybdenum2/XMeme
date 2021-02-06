2ec8efede5f16a3e9a8289edad759f1e1fde6aad
4molybdenum2 <tathagatapaul7@gmail.com>
2021-02-06 19:53:53 +0530

    changes started script

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const morgan = require('morgan')
 const app = express();
 
 if(process.env.NODE_ENV == 'development'){
-    dotenv.config({path: './config/config.env'})
+    dotenv.config({path: './.env'})
 }
 
 if(process.env.NODE_ENV == 'development'){
