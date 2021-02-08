cf32b705a3567e5097eeaaec265129b19330635d
4molybdenum2 <tathagatapaul7@gmail.com>
2021-02-08 14:51:39 +0530

    some changes

--- install.sh
+++ install.sh
@@ -3,7 +3,18 @@
 
 # Any installation related commands
 
-sudo apt-get install -y npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
+. ~/.nvm/nvm.sh
+
+nvm install node
+
+node -v
+
+npm -v
+
+sudo apt-get install -y git mongodb
+
+git clone https://gitlab.crio.do/COHORT_ME_BUILDOUT_XMEME_ENROLL_1612436694845/tathagatapaul7-me_buildout_xmeme
 
 # Any configuration related commands
